9669f266d55249a54aefd9bc70023e6e618bef9e
KH-boop-bit <152586072+KH-boop-bit@users.noreply.github.com>
2025-05-30 22:52:30 +0800

    update files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nma=xi6x2p-crjg^ifqqkapyu1qjd0l=+wn)-rijk_o%$!k3w_"
-
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -99,9 +99,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nomnoms_db',
+        'NAME': 'yourdbname',
         'USER': 'root',
-        'PASSWORD': 'Yuzusqueeze01!',
+        'PASSWORD': 'yourdbpassword',
         'HOST': '127.0.0.1', 
         'HOST': 'localhost', 
         'PORT': '3306'
