085567b375aec0b374181720c57b9431504a7922
Keng-Hong <152586072+KH-boop-bit@users.noreply.github.com>
2025-06-18 10:20:59 +0800

    Small request
    Just a small update to continue building off social,
    
    require update to cuisine options
    
    update to post as a review as well

--- backend/social/models.py
+++ backend/social/models.py
@@ -8,9 +8,16 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     image = models.ImageField(upload_to="post_images/", blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True) #display if present
+    place_id = models.CharField(max_length=255, blank=True, null=True) #dont display, backend purpose only
+    rating = models.IntegerField(blank=True, null=True) #limit this from 1 to 5, display if present
 
     def __str__(self):
         return self.title
+    
+    @property
+    def is_review(self): #only a review if location and rating is filled
+        return self.location is not None and self.rating is not None
 
 # new Like model that will be associated with each post and user
 class Like(models.Model):
